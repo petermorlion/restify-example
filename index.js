c85e5f7cb3518c5cc7f3808b82bba8029b98a72e
Peter Morlion <peter.morlion@gmail.com>
2019-02-08 09:39:36 +0100

    Add GET endpoints for teams and games

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-var restify = require('restify');
+const restify = require('restify');
 
-var server = restify.createServer();
+const server = restify.createServer();
+
+const inMemoryCollection = {
+    teams: [],
+    games: []
+};
+
+server.get("/teams", (req, res, next) => {
+    res.send(inMemoryCollection.teams);
+    next();
+});
+
+server.get("/games", (req, res, next) => {
+    res.send(inMemoryCollection.teams);
+    next();
+});
 
 server.listen(8080, function () {
     console.log('%s listening at %s', server.name, server.url);
-})
+}); 
