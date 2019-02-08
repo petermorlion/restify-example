224404d6a447d60da0209364f239d94ed7b40ed1
Peter Morlion <peter.morlion@gmail.com>
2019-02-08 10:15:27 +0100

    Add other endpoints

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 const restify = require('restify');
-
-const server = restify.createServer();
+const uuid = require("uuid/v4");
 
 const inMemoryCollection = {
     teams: [],
     games: []
 };
 
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
 server.get("/teams", (req, res, next) => {
-    res.send(inMemoryCollection.teams);
-    next();
+    res.send({ data: inMemoryCollection.teams });
+    return next();
+});
+
+server.post("/teams", (req, res, next) => {
+    req.body.id = uuid();
+    inMemoryCollection.teams.push(req.body);
+    res.send(201);
+    return next();
 });
 
 server.get("/games", (req, res, next) => {
-    res.send(inMemoryCollection.teams);
-    next();
+    res.send({ data: inMemoryCollection.games });
+    return next();
+});
+
+server.post("/games", (req, res, next) => {
+    req.body.id = uuid();
+    inMemoryCollection.games.push(req.body);
+    res.send(201);
+    return next();
+});
+
+server.get({name: "team", path: "/teams/:id"}, (req, res, next) => {
+    const team = inMemoryCollection.teams.filter(value => value.id === req.params.id);
+    res.send(team);
+    return next();
+});
+
+server.get("/games/:id", (req, res, next) => {
+    const game = inMemoryCollection.games.filter(value => value.id === req.params.id);
+    res.send({
+        id: game.id,
+        homeTeam: server.router.render("team", { id: game.homeTeamId }),
+        awayTeam: server.router.render("team", { id: game.awayTeamId })
+    });
+    return next();
 });
 
 server.listen(8080, function () {
